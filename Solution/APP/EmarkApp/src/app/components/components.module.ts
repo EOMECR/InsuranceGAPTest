6ebb25f8c7c11f8057711b1372179475ba3a6ba4
emfobla@gmail.com <>
2020-07-15 08:59:48 -0600

    serchbar seed

--- Solution/APP/EmarkApp/src/app/components/components.module.ts
+++ Solution/APP/EmarkApp/src/app/components/components.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    SearchbarComponent
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     CommonModule
---
+++ Solution/APP/EmarkApp/src/app/components/searchbar/searchbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.scss'],
+})
+export class SearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
